a670aac1570c9dc646f0e02b8c246224cf4d7fb1
newExpand <magin0auto@gmail.com>
2023-09-05 15:50:23 +0900

    파일 업로드 api 만든 후 테스트 하기 테스트1: 가상의 파일에 있는 내용 업로드 테스트 테스트2: 실제 txt파일로 업로드 테스트

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,11 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
 import { AppService } from './app.service';
 
 @Controller()
@@ -8,5 +15,13 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Post('file-upload')
+  @UseInterceptors(FileInterceptor('file'))
+  fileUpload(@UploadedFile() file: Express.Multer.File) {
+    console.log(file.buffer.toString('utf-8'));
+
+    return 'File Upload';
   }
 }
